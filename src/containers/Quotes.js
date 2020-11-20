843df4cd30a0ed6975796dc2755e52d55ad087be
wickbushong <wickbushong@gmail.com>
2020-11-20 13:59:59 -0600

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote } from '../actions/quotes'
+import { upvoteQuote } from '../actions/quotes'
+import { downvoteQuote } from '../actions/quotes'
+
+
 
 class Quotes extends Component {
 
+  renderCards = () => {
+    const { quotes, removeQuote, upvoteQuote, downvoteQuote } = this.props;
+    return quotes.map(q => {
+      return <QuoteCard 
+      quote={q}
+      // key={q.id}
+      upvoteQuote={upvoteQuote}
+      removeQuote={removeQuote}
+      downvoteQuote={downvoteQuote}
+      />
+    })
+  }
+
   render() {
+    
     return (
       <div>
         <hr />
@@ -20,6 +39,7 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+              {this.renderCards()}
             </div>
           </div>
         </div>
@@ -28,5 +48,17 @@ class Quotes extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {quotes: state.quotes}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    removeQuote: quoteId => {dispatch(removeQuote(quoteId))},
+    upvoteQuote: quoteId => {dispatch(upvoteQuote(quoteId))},
+    downvoteQuote: quoteId => {dispatch(downvoteQuote(quoteId))}
+  }
+}
+
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, mapDispatchToProps)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,29 @@
 export default (state = [], action) => {
-  return state;
+  console.log(action)
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote)
+    case "REMOVE_QUOTE":
+      return state.filter(q => q.id != action.quoteId)
+    case "UPVOTE_QUOTE":
+      return (
+        state.map(q => {
+          if (q.id === action.quoteId) {
+            return {...q, votes: q.votes + 1}
+          }
+        })
+      )
+    case "DOWNVOTE_QUOTE":
+      return (
+        state.map(q => {
+          if (q.id === action.quoteId && q.votes != 0) {
+            return {...q, votes: q.votes - 1}
+          } else {
+            return q
+          }
+        })
+      )
+    default:
+      return state;
+  }
 }
